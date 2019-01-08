2d5d7413f2675fad7ac575e8848ef9c4baa82869
smd9788 <smd09876@gmail.com>
2019-01-08 14:55:56 -0500

    new game array's now collecting index numbers properly

--- assets/scripts/gameboard.js
+++ assets/scripts/gameboard.js
@@ -1,44 +1,63 @@
-// start with blank gameboard gameboard array
-const gameBoardArray = []
-const startTurn = 0
-// const p1select = document.getElementById('player-one-select')
-// const p2select = document.getElementById('player-two-select')
-// choose player
-// const choosePlayer = (event) => {
-//                          players array. X and O are the values
-//   const players = [selectP1.value, selectP2.value]
-//   const selection = (click) => {
-//   }
-// }
+// start by assigning the cells in our gameboard
+const cell0 = document.getElementById('cell-zero').id
+const cell1 = document.getElementById('cell-one').id
+const cell2 = document.getElementById('cell-two').id
+const cell3 = document.getElementById('cell-three').id
+const cell4 = document.getElementById('cell-four').id
+const cell5 = document.getElementById('cell-five').id
+const cell6 = document.getElementById('cell-six').id
+const cell7 = document.getElementById('cell-seven').id
+const cell8 = document.getElementById('cell-eight').id
 
-// set up click handler to push player move to array and render a game piece
+// create gameboard array
+const gameBoardArray = [cell0, cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8]
+// blank array to collect X's moves
+let newGameXArray = []
+// blank array to collect O's moves
+let newGameOArray = []
+
+let turnNumber = 0
+// set up click handler to push player move to their array and render a game piece
 const clickSquare = (event) => {
   const move = event.target.id
   const placeXpiece = () => {
-    gameBoardArray.push('X')
+    // determine index number of square selected
+    newGameXArray[turnNumber] = gameBoardArray.indexOf(move)
     document.getElementById(move).append('X')
+    turnNumber += 1
+    // filter to remove empty values from array
+    newGameXArray = newGameXArray.filter(function (notempty) {
+      return typeof (notempty) === 'number'
+    })
+    console.log('X array is:', newGameXArray)
+    checkGameOver(newGameXArray)
   }
   const placeOpiece = () => {
-    gameBoardArray.push('O')
+    newGameOArray[turnNumber] = gameBoardArray.indexOf(move)
     document.getElementById(move).append('O')
+    turnNumber += 1
+    newGameOArray = newGameOArray.filter(function (notempty) {
+      return typeof (notempty) === 'number'
+    })
+    console.log('O array is:', newGameOArray)
+    checkGameOver(newGameOArray)
   }
-  if (gameBoardArray.length % 2) {
+  // logic to know when to play X or O
+  if (turnNumber % 2) {
     placeOpiece()
   } else {
     placeXpiece()
   }
-  console.log('Gameboard array is:', gameBoardArray)
+  // console.log('X array is currently:', newGameXArray)
+  // console.log('O array is currently:', newGameOArray)
+  // console.log(newGameOArray.filter(function (notempty) {
+  //   return typeof (notempty) === 'number'
+  // }))
+  return gameBoardArray
 }
 
-// // setup click handler to display 'X' in cell on click
-// const placeGameMarker = (gameBoard) => {
-//   if (gameBoard.gameBoardArray.length === ) {
-//
-//   }
-// }
-
 // set up win conditions
-function checkGameOver (gameBoard) {
+function checkGameOver (array) {
   const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -49,11 +68,17 @@ function checkGameOver (gameBoard) {
     [0, 4, 8],
     [2, 4, 6]
   ]
-  return winConditions
+  winConditions.forEach(function (winCombo) {
+    if (newGameXArray === winCombo) {
+      console.log('X Wins the Game!')
+    } else if (newGameXArray === winCombo) {
+      console.log('O Wins the Game!')
+    }
+  })
+  return true
 }
 
 module.exports = {
   checkGameOver,
-  clickSquare,
-  startTurn
+  clickSquare
 }
