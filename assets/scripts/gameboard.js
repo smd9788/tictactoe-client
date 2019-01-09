255f07a0ec09791bd4457ca78f307102868bbedc
smd9788 <smd09876@gmail.com>
2019-01-09 11:34:17 -0500

    Added game logic. Game now appends X's and O's. Game also checks for winner and displays message.

--- assets/scripts/gameboard.js
+++ assets/scripts/gameboard.js
@@ -7,15 +7,98 @@ const gameBoard = (function () {
       const gameBoardCell = document.createElement('div')
       gameBoardCell.className = 'grid-item'
       gameBoardCell.id = i
-      // setup click action to start the game. then initilize function
+      // this click action starts the game. function initialized below
+      gameBoardCell.addEventListener('click', () => {
+        gamePlay.playTurn(parseInt(gameBoardCell.id, 10))
+      })
       gameBoardContainer.appendChild(gameBoardCell)
     }
   }
-  return { drawGameBoard }
+  return {
+    drawGameBoard
+  }
 })()
 
 gameBoard.drawGameBoard()
 
+// get the cells we just created in the drawGameBoard function
+const gameBoardCells = document.getElementsByClassName('grid-item')
+// game logic
+const gamePlay = (function () {
+  const gridCellsPlayed = []
+  let playerTurn = 1
+  let playerHasWon = false
+  const victoryMessage = document.getElementById('victory-message')
+  const playTurn = function (score) {
+    if (playerTurn === 1 && playerHasWon === false && !gridCellsPlayed.includes(score)) {
+      playerOne.score.push(score)
+      gridCellsPlayed.push(score)
+      gamePlay.checkForWin(gamePlay.winConditions, playerOne.score, 'one')
+      drawXO(gameBoardCells[score])
+      playerTurn = 2
+      return playerTurn
+    }
+    if (playerTurn === 2 && playerHasWon === false && !gridCellsPlayed.includes(score)) {
+      playerTwo.score.push(score)
+      gridCellsPlayed.push(score)
+      gamePlay.checkForWin(gamePlay.winConditions, playerTwo.score, 'two')
+      drawXO(gameBoardCells[score])
+      playerTurn = 1
+      return playerTurn
+    }
+  }
+
+  const drawXO = function (gameBoardCell) {
+    const boardMarker = document.createElement('h1')
+    if (playerTurn === 1) {
+      boardMarker.innerHTML = 'X'
+    } else {
+      boardMarker.innerHTML = 'O'
+    }
+    gameBoardCell.appendChild(boardMarker)
+  }
+
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ]
+
+  function checkForWin (winConditions, playerScore, playerWinner) {
+    for (let i = 0; i < winConditions.length; i++) {
+      if (arrayContainsArray(winConditions[i], playerScore) === true) {
+        playerHasWon = true
+        victoryMessage.innerHTML = 'Player ' + playerWinner + ' wins!'
+        return console.log('Player ' + playerWinner + ' wins!')
+      }
+    }
+  }
+
+  return { gridCellsPlayed, playTurn, checkForWin, winConditions, drawXO }
+})()
+
+const playerOne = (function () {
+  const score = []
+  return { score }
+})()
+
+const playerTwo = (function () {
+  const score = []
+  return { score }
+})()
+
+function arrayContainsArray (arrayOne, arrayTwo) {
+  return arrayOne.every(function (value) {
+    return arrayTwo.indexOf(value) > -1
+  })
+}
 module.exports = {
-  gameBoard
+  gameBoard,
+  gameBoardCells,
+  gamePlay
 }
